9d71ab690698a429e24d25f59773a90fb62827ef
rokaut990 <64099437+rokaut990@users.noreply.github.com>
2021-03-14 17:44:42 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,41 +1,73 @@
-module.exports = function toReadable (number) {
-    var ones = new Array('', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine',);
-    var teens = new Array('ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen')
-    var tens = new Array('', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety');
-    var hundred = ' hundred';
-    var output = '';
+module.exports = function toReadable(number) {
+    var ones = new Array(
+        "",
+        " one",
+        " two",
+        " three",
+        " four",
+        " five",
+        " six",
+        " seven",
+        " eight",
+        " nine"
+    );
+    var teens = new Array(
+        " ten",
+        " eleven",
+        " twelve",
+        " thirteen",
+        " fourteen",
+        " fifteen",
+        " sixteen",
+        " seventeen",
+        " eighteen",
+        " nineteen"
+    );
+    var tens = new Array(
+        "",
+        "ten",
+        " twenty",
+        " thirty",
+        " forty",
+        " fifty",
+        " sixty",
+        " seventy",
+        " eighty",
+        " ninety"
+    );
+    var hundred = " hundred";
+    var output = "";
     var numberString = number.toString();
 
-    if (number == 0) {
-        return 0;
+    if (!number) {
+        return "zero";
     }
-    
+
     if (number < 10) {
-        output = ones[number];
-        return output.trim();  
+        return ones[number].trim();
     }
-    let i = number % 100;
-    if (i > 9 && i < 20){
-        output = ones[parseInt(numberString.charAt(0))] + hundred;
-        output += teens[parseInt(numberString.charAt(2))];
-        return output.trim();
-    }
-    
+    if (number === 10) return "ten";
     if (number > 9 && number < 20) {
-        output = teens[number];
+        output += teens[numberString[1]];
         return output.trim();
     }
 
-    
     if (numberString.length == 3) {
-        output = ones[parseInt(numberString.charAt(0))] + hundred;
-        output += tens[parseInt(numberString.charAt(1))];
-        output += ones[parseInt(numberString.charAt(2))];
+        let superNumber = number % 100;
+        if (superNumber > 9 && superNumber < 20) {
+            output = ones[numberString[0]] + hundred;
+            output = ones[numberString[0]] + hundred;
+            output += teens[numberString[2]];
+            return output.trim();
+        }
+        output = ones[numberString[0]] + hundred;
+        output += tens[numberString[1]];
+        output += ones[numberString[2]];
         return output.trim();
     }
 
-    output += tens[parseInt(numberString.charAt(0))];
-    output += ones[parseInt(numberString.charAt(1))];
+    output += tens[numberString.charAt(0)];
+    output += ones[numberString.charAt(1)];
 
     return output.trim();
-}   
+};
